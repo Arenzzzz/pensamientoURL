34c0805787bfa010a9b39e58f3d16f182f70f7bb
Arenzzzz <varenzp024@gmail.com>
2025-03-05 11:27:45 -0600

    EJERCICIO_3-4.py

--- EJERCICIO_3-4.py
+++ EJERCICIO_3-4.py
@@ -19,3 +19,34 @@ c = texto.split()
 a = ' '.join(c)
 
 print(a)
+
+#EJERCICIO 3
+mail = str(input("Ingrese un correo: "))
+print()
+
+dominio = mail.split('@')
+a = dominio[1]
+
+print(f"El dominio es: {a}")
+
+#EJERCICIO 4
+file = str(input("Ingrese un archivo: "))
+print()
+
+tipo = file.split('.')
+a = tipo[1]
+
+val = bool(a == 'pdf')
+
+print(f"¿El archivo es .pdf? = {val}")
+
+#EJERCICIO 5
+texto = str(input("Ingrese una oración: "))
+print()
+
+a = texto.split()
+b = a[::-1]
+c = ' '.join(b)
+
+print(c)
+
