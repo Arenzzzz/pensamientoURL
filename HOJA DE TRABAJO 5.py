fe398ddf03a2410723bded73b8c3b3ab21915b03
Arenzzzz <varenzp024@gmail.com>
2025-03-12 11:04:38 -0600

    Update HOJA DE TRABAJO 5.py

--- HOJA DE TRABAJO 5.py
+++ HOJA DE TRABAJO 5.py
@@ -1,14 +1,14 @@
 #EJERCICIO 1
 print("CÁLCULO DE SERVICIO DE AGUA \n")
 
-consumo = float(input("Ingrese el consumo de agua en m³: "))
-h = int(input("Ingrese el número de habitantes en el área: "))
+consumo = float(input("Ingrese el consumo de agua en m³: "))        #LEER EL CONSUMO
+h = int(input("Ingrese el número de habitantes en el área: "))      #LEER NÚMERO DE HABITANTES
 print()
 
-if (consumo<15):
+if (consumo<15):    #Si el consumo es menor a 15 m³
     print(f"La tarifa es de Q{5*consumo}")
 
-elif (consumo>=15 and consumo<=30):
+elif (consumo>=15 and consumo<=30): #Si el consumo está entre 15 y 30 m³
     if(h>3):
         print(f"La tarifa es de Q{4*consumo}")
     elif(h==3):
@@ -16,7 +16,7 @@ elif (consumo>=15 and consumo<=30):
     else:
         print(f"La tarifa es de Q{5*consumo}")
 
-elif (consumo>30):
+elif (consumo>30):  #Si el consumo es mayor a 30 m³
     if(h>5):
         print(f"La tarifa es de Q{3.5*consumo}")
     elif(h%2==0):
