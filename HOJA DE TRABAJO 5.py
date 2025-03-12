f3540978e7839beb83480cc3cb8b7e8903a3eb46
Arenzzzz <varenzp024@gmail.com>
2025-03-12 11:28:23 -0600

    Update HOJA DE TRABAJO 5.py

--- HOJA DE TRABAJO 5.py
+++ HOJA DE TRABAJO 5.py
@@ -1,3 +1,5 @@
+#Hecho por: ARENZ PELÁEZ - 1556425
+
 #EJERCICIO 1
 print("CÁLCULO DE SERVICIO DE AGUA \n")
 
@@ -23,3 +25,28 @@ elif (consumo>30):  #Si el consumo es mayor a 30 m³
         print(f"La tarifa es de Q{4*consumo}")
     else:
         print(f"La tarifa es de Q{4.2*consumo}")
+
+#EJERCICIO 2
+print("REGLAMENTO PARA REGULACIÓN DE TRÁFICO \n")
+
+año = int(input("Ingrese el año del modelo del vehículo: "))       #LEER AÑO DEL VEHÍCULO
+placa = input("Ingrese la placa del vehículo: ")                   #LEER PLACA DEL VEHÍCULO
+print()
+
+d =  int(placa[-1])    #CONVERTIR ÚLTIMO DÍGITO A ENTERO
+
+if (año>=2001):    #Si el vehículo es del 2001 en adelante
+    if(d%2==0):
+        if(año%2==0):
+            print("El vehículo NO circula los lunes y sábados circula hasta el mediodía")
+        else:
+            print("El vehículo NO circula los lunes")
+            
+    elif(d%2!=0):
+        if(año%2==0):
+            print("El vehículo NO circula los viernes y sábados circula hasta el mediodía")
+        else:
+            print("El vehículo NO circula los viernes")
+    
+else:
+    print("ADVERTENCIA: Realice mantenimiento al vehículo")
