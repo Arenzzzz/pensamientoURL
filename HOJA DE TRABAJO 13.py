426cd7aaf6f62bbe6bdc3a92f2007aee104eb963
Arenzzzz <varenzp024@gmail.com>
2025-05-21 11:29:47 -0600

    HOJA DE TRABAJO 13.py

--- HOJA DE TRABAJO 13.py
+++ HOJA DE TRABAJO 13.py
@@ -18,3 +18,23 @@ def imprimir_tablero(a):
         print()
 
 imprimir_tablero(matriz)
+
+m = [1,0,1,1,0]
+m2 = []
+
+for i in m:
+    b = m.index(i)
+    if i == 1:
+        if (m[b-1] == 0 and m[b+1] == 1) or (m[b-1] == 1 and m[b+1] == 0):
+            m2.append(1)
+        elif m[b-1] == 0 and m[b+1] == 0:
+            m2.append(0)
+        elif m[b-1] == 1 and m[b+1] == 1:
+            m2.append(0)
+    elif i == 0:
+        if ((m[b-1] == 1 and m[b+1] == 0) or (m[b-1] == 0 and m[b+1] == 1)):
+            m2.append(1)
+        else:
+            m2.append(0)
+        
+print(m2)
